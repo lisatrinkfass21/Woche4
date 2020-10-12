624a87aba268bb3d5ef83b80f10463ca00a87c7b
Trinkfass Lisa <Trinkfass.Lisa@sus.htl-grieskirchen.at>
2020-10-12 14:52:19 +0200

    einlesen csv-File

---
+++ Beispiel1/src/beispiel1/Beispiel1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beispiel1;
+
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author Lisa
+ */
+public class Beispiel1 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        NumberReader nr = new NumberReader();
+        nr.readFile();
+
+    }
+
+}
---
+++ Beispiel1/src/beispiel1/NumberReader.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beispiel1;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Lisa
+ */
+public class NumberReader {
+
+    ArrayList<Integer> list = new ArrayList<Integer>();
+
+    public void readFile() throws FileNotFoundException {
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("numbers.csv"));
+            String numbers;
+            while ((numbers = br.readLine()) != null) {
+                String[] numb = numbers.split(":");
+                for (String string : numb) {
+                    isNumeric(string);
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(NumberReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private void isNumeric(String num) {
+        int zahl;
+        try {
+            zahl = Integer.parseInt(num);
+            list.add(zahl);
+        } catch (Exception e) {
+
+        }
+
+    }
+
+}
